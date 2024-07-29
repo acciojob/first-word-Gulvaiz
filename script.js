b82edabdb374e7983002cdecbb750e2f54a4d1cf
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 02:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function firstWord(s) {
+  // Trim leading and trailing whitespace
+  s = s.trim();
   
-  s = s + " ";
+  // Initialize an empty temporary string
   let temp = "";
-  
-  for (let i = 0; i < s.length; i++) {
-    if (s.charAt(i) !== " ") {
-      temp += s.charAt(i);
+
+  for (const char of s) {
+    if (char !== " ") {
+      temp += char;
     } else {
-      
+      // Return the first word as soon as we encounter a space
       return temp;
     }
   }
 
-  return temp;  
+  return temp; // This return statement is for strings with no spaces at all
 }
 
 // Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
 
