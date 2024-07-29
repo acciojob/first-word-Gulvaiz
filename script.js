fa0b75957fd54002c127bd268a53d03bde099357
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-07-30 02:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function firstWord(s) {
-  // your code here
-	 let s = s + " "
-	  let temp = " "
-   for(let i=0; i<s.length; i++){
-   
-        if((s.charAt(i) != " "){
-            temp += s.charAt(i)
-        }
-        else{
-           return temp
-        }
-    return temp;
+  
+  s = s + " ";
+  let temp = "";
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) !== " ") {
+      temp += s.charAt(i);
+    } else {
+      
+      return temp;
+    }
+  }
+
+  return temp;  
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
